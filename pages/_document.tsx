acd51a953b529b9fb2812d365895eb86f662b662
TheOutdoorProgrammer <joey@theoutdoorprogrammer.com>
2025-12-28 13:14:38 -0500

    feat: ai instructions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import {Html, Head, Main, NextScript} from 'next/document'
+import { llmInstructions } from '@gremllm/nextjs/metadata';
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head/>
+            <Head>
+                <meta name="llm-instructions" content={llmInstructions} />
+            </Head>
             <body>
             <Main/>
             <NextScript/>
